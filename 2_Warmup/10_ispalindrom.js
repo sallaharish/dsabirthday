5360909dd23afa11262d74ac2cb266aa6ec1cf57
Salla Harish <sallaharish040@gmail.com>
2026-01-15 15:03:14 +0530

    day4

---
+++ 2_Warmup/10_ispalindrom.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if(x<0)return false
+  
+  let xcopy=x;
+  let rev=0;
+  while(x>0){
+    let rem= x%10;
+    rev=(rev*10)+rem
+    x=Math.floor(x/10)
+  }
+  return xcopy===rev;
+};
