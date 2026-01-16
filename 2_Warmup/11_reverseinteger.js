5edda6a7a25f99a64e34802782521e42da4872e8
Salla Harish <sallaharish040@gmail.com>
2026-01-16 14:45:31 +0530

    day5

---
+++ 2_Warmup/11_reverseinteger.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let xcopy=x;
+    x=  Math.abs(x);
+    let rev=0;
+    while(x>0){
+        let last=x%10;
+        rev=(10*rev)+last
+        x=Math.floor(x/10)
+    }
+    let limit = 2**31;
+    if(rev<-limit || rev> limit-1){
+        return 0
+    }
+    else{
+        return xcopy<0 ? -rev:rev
+    }
+    
+};
