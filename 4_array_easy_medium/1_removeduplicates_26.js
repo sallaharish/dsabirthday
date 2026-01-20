c0811f805ddf8701f5eae7c0e51e971cc815959b
Salla Harish <sallaharish040@gmail.com>
2026-01-20 18:56:14 +0530

    day6

--- 4_array_easy_medium/1_removeduplicates_26.js
+++ 4_array_easy_medium/1_removeduplicates_26.js
@@ -6,8 +6,9 @@ var removeDuplicates = function(nums) {
     let x=0
     for(let i=0;i<nums.length;i++){
         if(nums[i]>nums[x]){
+            
+            nums[x+1]=nums[i]
              x=x+1
-            nums[x]=nums[i]
            
         }
     }
